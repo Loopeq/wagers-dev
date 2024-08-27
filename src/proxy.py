f61262870640070df27d12d21b9345cfea4f249a
Loopeq <arsen.00z11@gmail.com>
2024-08-28 00:16:28 +0300

    feature: fetch straight init logic

--- src/proxy.py
+++ src/proxy.py
@@ -1,62 +1,44 @@
 from webshare import ApiClient
-from webshare.util import Proxy
 
-from settings import settings as conf
-from dataclasses import dataclass
+from src.settings import settings as conf
 
 
-@dataclass
-class ProxyModel:
-    data: list[Proxy]
-    count: int
-
-
-class InvalidProxy(Exception):
+class NoValidProxyError(Exception):
     """Plug"""
     pass
 
 
 class ProxyManager:
-    SCHEMA = 'http://{0}:{1}@{2}:{3}'
+    # (0) username (1) password (2) ip (3) port
+    _SCHEMA = 'http://{0}:{1}@{2}:{3}'
+    _CLIENT = ApiClient(conf.WEBSHARE_API)
 
     def __init__(self):
-        self.current = None
+        self._proxy_list = self._CLIENT.get_proxy_list().get_results()
         self._current_id = 0
-        self._client = ApiClient(conf.WEBSHARE_API)
-        self.proxies: ProxyModel = self._get_proxy_list()
-        self.update()
 
-    def _get_proxy_list(self):
-        response = self._client.get_proxy_list()
-        proxies = response.get_results()
-        count = response.count
-        return ProxyModel(proxies, count)
+    def _fetch_proxy(self):
+        return self._proxy_list[self._current_id]
 
-    def _fetch_proxy(self) -> str | None:
-        # (0) username (1) password (2) ip (3) port
-        if self._current_id < self.proxies.count:
-            proxy = self.proxies.data[self._current_id]
-            if proxy.valid:
-                return self.SCHEMA.format(proxy.username,
-                                          proxy.password,
-                                          proxy.proxy_address,
-                                          proxy.port)
-            raise InvalidProxy(f'{proxy.__dict__} - is invalid')
-        raise IndexError('_current_id out of range')
+    @property
+    def proxy(self) -> str:
+
+        proxy = self._fetch_proxy()
+        proxy_list_length = len(self._proxy_list)
+
+        while not proxy.valid:
+            self._current_id += 1
+            if self._current_id >= proxy_list_length - 1:
+                raise NoValidProxyError('All of your proxies are invalid')
+            proxy = self._fetch_proxy()
+
+        return self._SCHEMA.format(proxy.username,
+                                   proxy.password,
+                                   proxy.proxy_address,
+                                   proxy.port)
 
     def update(self):
-        while True:
-            try:
-                self._current_id += 1
-                self.current = self._fetch_proxy()
-                break
-            except InvalidProxy as e:
-                print(e)
-                self._current_id += 1
-                self.current = self._fetch_proxy()
-            except IndexError as e:
-                print(e)
-                self._current_id = 0
+        self._current_id += 1
 
 
-
+proxy_manager = ProxyManager()
