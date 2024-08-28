a2f96abe95183cbe719ab5228471a4cbe94a5fdc
Loopeq <arsen.00z11@gmail.com>
2024-08-28 19:14:42 +0300

    add scheduler logic

--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
+from downloader import run_downloader
 import asyncio
-import requests
 
-"""curl 'https://guest.api.arcadia.pinnacle.com/0.1/matchups/1595892404/markets/related/straight' \
-  -H 'sec-ch-ua: "Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"' \
-  -H 'X-Device-UUID: 6d702fee-ff3e303d-e0986d30-b379cb05' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36' \
-  -H 'Content-Type: application/json' \
-  -H 'Accept: application/json' \
-  -H 'Referer: https://www.pinnacle.com/' \
-  -H 'X-API-Key: CmX2KcMrXuFmNg6YFbmTxE0y9CIrOi0R' \
-  -H 'sec-ch-ua-platform: "Linux"'"""
 
-"""
-"""
 async def main():
-    pass
+    await run_downloader()
 
 
 if __name__ == "__main__":
