ad7f3bde86758a5e265dc73a96759360b3ceb848
Loopeq <arsen.00z11@gmail.com>
2024-10-18 23:19:50 +0300

    change db port

--- src/data/crud.py
+++ src/data/crud.py
@@ -29,7 +29,6 @@ from sqlalchemy.orm import Session
 
 async def create_tables():
     async with async_engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
