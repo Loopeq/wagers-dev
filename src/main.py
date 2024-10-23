af61146b08e1dbb7f0d8614c286c4e5dd2009977
Loopeq <arsen.00z11@gmail.com>
2024-10-23 23:01:42 +0300

    stable

--- src/main.py
+++ src/main.py
@@ -1,61 +1,55 @@
-# import asyncio
-# import datetime
-# from contextlib import asynccontextmanager
-# import uvicorn
-# from fastapi import FastAPI, WebSocket, Depends, WebSocketDisconnect
-# from fastapi.responses import HTMLResponse
-# from typing import List, Optional
-# import json
-# from starlette.middleware.cors import CORSMiddleware  # NEW
-# from src.api.provider import ApiOrm
-# from src.api.schemas import FilterResponse, FilterRequest, filters
-# from src.data.crud import UpdateManager, create_tables
-# from src.data.schemas import SportDTO
-# from src.parser import parser
-# from src.data.crud import SportOrm
-from fastapi import FastAPI
+import asyncio
+import datetime
+from contextlib import asynccontextmanager
+import uvicorn
+from fastapi import FastAPI, WebSocket, Depends, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from typing import List, Optional
+import json
+from starlette.middleware.cors import CORSMiddleware  # NEW
+from src.api.provider import ApiOrm
+from src.api.schemas import FilterResponse, FilterRequest, filters
+from src.data.crud import UpdateManager, create_tables
+from src.data.schemas import SportDTO
+from src.parser import parser
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await create_tables()
-#     await asyncio.create_task(parser.run_parser())
-#     yield
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    await asyncio.create_task(parser.run_parser())
+    yield
 
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=['*'],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
-async def test():
-    return 'Hello world'
+async def get_match(filters: FilterRequest = Depends()):
+    matches = await ApiOrm.get_match_with_change(filters=filters)
+    return matches
 
 
-# @app.get('/')
-# async def get_match(filters: FilterRequest = Depends()):
-#     matches = await ApiOrm.get_match_with_change(filters=filters)
-#     return matches
-#
-#
-# @app.get('/filters', response_model=FilterResponse)
-# async def get_filters():
-#     return filters
-#
-#
-# @app.get('/match/{match_id}')
-# async def get_point_change(match_id: int):
-#     changes = await ApiOrm.get_point_change(match_id=match_id)
-#     return changes
-#
-#
-# @app.get('/history/{team_name}')
-# async def get_team_history(team_name: str, current_match_id: int):
-#     team_info = await ApiOrm.get_match_history_by_team_name(team_name=team_name, current_match_id=current_match_id)
-#     return team_info
+@app.get('/filters', response_model=FilterResponse)
+async def get_filters():
+    return filters
+
+
+@app.get('/match/{match_id}')
+async def get_point_change(match_id: int):
+    changes = await ApiOrm.get_point_change(match_id=match_id)
+    return changes
+
+
+@app.get('/history/{team_name}')
+async def get_team_history(team_name: str, current_match_id: int):
+    team_info = await ApiOrm.get_match_history_by_team_name(team_name=team_name, current_match_id=current_match_id)
+    return team_info
