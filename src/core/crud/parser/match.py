9a2696851a0ed34c80764efa26316d8a22b24864
mirzaev.arsen <mirzaev.a@cadesign.ru>
2025-12-21 17:06:05 +0300

    Revert "fix: Убарл бота"
    This reverts commit 554404a78274b1d891c0c6f6d4cffc1ff4d62478.

--- src/core/crud/parser/match.py
+++ src/core/crud/parser/match.py
@@ -122,6 +122,7 @@ async def clear_events_by_start_time():
             .join(League, Match.league_id == League.id)
             .where(
                 Match.start_time < current_time - timedelta(days=int(clear_threshold)),
+                League.sport_id != sports['tennis']
             )
         )
 
