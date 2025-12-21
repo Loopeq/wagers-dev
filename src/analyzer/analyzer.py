dcfa8cf447a391f57cfb13edccdfa59ee6447fbf
mirzaev.arsen <mirzaev.a@cadesign.ru>
2025-12-21 17:26:41 +0300

    change providers

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -1,9 +1,9 @@
 from g4f.client import AsyncClient
 from src.analyzer.promt import ROLE
-from g4f.Provider import RetryProvider, Blackbox, LegacyLMArena, PollinationsAI, WeWordle, Yqcloud
+from g4f.Provider import RetryProvider, Bing, GeekGpt, Liaobots, Phind, Raycast
 
 async def get_analyzed(content: str, tool_call_query: str):
-    client = AsyncClient(provider=RetryProvider([Blackbox, LegacyLMArena, PollinationsAI, WeWordle, Yqcloud], shuffle=True))
+    client = AsyncClient(provider=RetryProvider([Bing, GeekGpt, Liaobots, Phind, Raycast], shuffle=True))
     tool_call = {
         "type": "function",
         "function": {
@@ -20,7 +20,7 @@ async def get_analyzed(content: str, tool_call_query: str):
     }
     
     response = await client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4",
         messages=[{"role": "developer", "content": ROLE},
                   {"role": "user", "content": content}],
         tool_call=tool_call
